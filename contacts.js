0297db697f19047efa0c428cc506e6e51d8ef0d9
Bohdan <yashbon@ukr.net>
2023-08-11 22:10:10 +0300

    completed tasks

--- contacts.js
+++ contacts.js
@@ -1,26 +1,73 @@
+// contacts.js
 const fs = require("fs/promises");
 const path = require("path");
-
-// contacts.js
+const crypto = require("node:crypto");
+// const crypto = require("crypto");
+// const { nanoid } = require("nanoid");
 
 /*
  * Розкоментуй і запиши значення
- * const contactsPath = ;
  */
+const contactsPath = path.join(__dirname, "db/contacts.json");
 
 // TODO: задокументувати кожну функцію
-function listContacts() {
-    // ...твій код. Повертає масив контактів.
+
+async function read() {
+    const data = await fs.readFile(contactsPath, "utf-8");
+
+    return JSON.parse(data);
 }
 
-function getContactById(contactId) {
-    // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+async function write(data) {
+    return fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
 }
 
-function removeContact(contactId) {
-    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+async function listContacts() {
+    //Повертає масив контактів.
+    const contacts = await read();
+
+    return contacts;
 }
 
-function addContact(name, email, phone) {
-    // ...твій код. Повертає об'єкт доданого контакту.
+async function getContactById(contactId) {
+    // Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+    const contacts = await read();
+    const contactById = contacts.find((contact) => contact.id === contactId);
+
+    return contactById ? contactById : null;
 }
+
+async function removeContact(contactId) {
+    // Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+    const contacts = await read();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+
+    if (!index) {
+        return null;
+    }
+
+    const newContacts = [
+        ...contacts.slice(0, index),
+        ...contacts.slice(index + 1),
+    ];
+    write(newContacts);
+
+    return contacts[index];
+}
+
+async function addContact(name, email, phone) {
+    // Повертає об'єкт доданого контакту.
+    const contacts = await read();
+    const newContact = { name, email, phone, id: crypto.randomUUID() };
+    contacts.push(newContact);
+    await write(contacts);
+
+    return newContact;
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-console.log("hello!");
+// const { error } = require("console");
+const contacts = require("./contacts");
+
+console.log(
+    "hello!"
+    // process.argv
+);
+
+// contacts.listContacts().then(console.log).catch(console.error);
+// contacts
+//     .getContactById("05olLMgyVQdWRwgKfg5J6")
+//     .then(console.log)
+//     .catch(console.error);
+
+// contacts
+//     .addContact("xaxa", "email@email.eml", "1212")
+//     .then(console.log)
+//     .catch(console.error);
+
+const { Command } = require("commander");
+const program = new Command();
+program
+    .option("-a, --action <type>", "choose action")
+    .option("-i, --id <type>", "user id")
+    .option("-n, --name <type>", "user name")
+    .option("-e, --email <type>", "user email")
+    .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторити
+function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case "list":
+            contacts.listContacts().then(console.log).catch(console.error);
+            break;
+
+        case "get":
+            contacts
+                .getContactById("05olLMgyVQdWRwgKfg5J6")
+                .then(console.log)
+                .catch(console.error);
+            break;
+
+        case "add":
+            // ... name email phone
+            contacts
+                .addContact(name, email, phone)
+                .then(console.log)
+                .catch(console.error);
+            break;
+
+        case "remove":
+            // ... id
+            contacts.removeContact(id).then(console.log).catch(console.error);
+            break;
+
+        default:
+            console.warn("\x1B[31m Unknown action type!");
+    }
+}
+
+invokeAction(argv);
